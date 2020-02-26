176302f587d9b0add0e1fc615d6efcadb01e7eb3
Kamil <kamil056@interia.pl>
2020-02-26 13:44:54 +0100

    +log in test

--- Pages/create_an_account.py
+++ Pages/create_an_account.py
@@ -7,13 +7,13 @@ class CreateAccountByPuttingEmail:
 
 
     # Create an account
-    EMAIL_INPUT_TO_CREATE = (By.ID, 'email_create')
-    EMAIL_BUTTON_TO_CREATE = (By.ID, 'SubmitCreate')
+    EMAIL_TO_CREATE = (By.ID, 'email_create')
+    BUTTON_TO_CREATE = (By.ID, 'SubmitCreate')
 
     # Already registered?
-    EMAIL_INPUT_TO_SIGN_IN = (By.ID, 'email')
-    PASSWORD_INPUT_TO_SIGN_IN = (By.ID, 'passwd')
-    EMAIL_BUTTON_TO_SIGN_IN = (By.ID, 'SubmitLogin')
+    EMAIL_TO_LOG_IN = (By.ID, 'email')
+    PASSWORD_TO_LOG_IN = (By.ID, 'passwd')
+    BUTTON_TO_LOG_IN = (By.ID, 'SubmitLogin')
     FORGOT_PASSWORD_LINK = (By.CLASS_NAME, 'lost_password form-group')
 
     def __init__(self, browser):
@@ -23,13 +23,21 @@ class CreateAccountByPuttingEmail:
         self.browser.get(self.URL)
 
     def register(self, email):
-        email_input_to_create = self.browser.find_element(*self.EMAIL_INPUT_TO_CREATE)
+        email_input_to_create = self.browser.find_element(*self.EMAIL_TO_CREATE)
         email_input_to_create.send_keys(email)
 
-        email_button_to_create = self.browser.find_element(*self.EMAIL_BUTTON_TO_CREATE)
+        email_button_to_create = self.browser.find_element(*self.BUTTON_TO_CREATE)
         email_button_to_create.click()
 
+    def log_in(self, user):
+        email_input_to_create = self.browser.find_element(*self.EMAIL_TO_LOG_IN)
+        email_input_to_create.send_keys(user.get("v_email"))
 
+        email_input_to_create = self.browser.find_element(*self.PASSWORD_TO_LOG_IN)
+        email_input_to_create.send_keys(user.get("v_password"))
+
+        email_button_to_create = self.browser.find_element(*self.BUTTON_TO_LOG_IN)
+        email_button_to_create.click()
 
 
 
--- Tests/test_authentication.py
+++ Tests/test_authentication.py
@@ -4,35 +4,36 @@ from Pages.create_an_account import CreateAccountByPuttingEmail
 from Pages.put_personal_information import YourPersonalInformation
 from selenium.webdriver import Firefox
 
+User1 = {
+    "v_first_name": "Kamik",
+    "v_last_name": "Hahahda",
+    "v_email": "kamil@hdvajjhaha.pl",
+    "v_password": "qwedrty",
+    "v_address": "Zidelna 123",
+    "v_city": "Wroclaw",
+    "v_postal": "12555",
+    "v_country": "21",
+    "v_mobile_phone": "89837484783",
+    "v_alias": "My Address 1d",
+    "v_gender": 2,
+    "v_day": "12",
+    "v_month": "1",
+    "v_year": "1972",
+    "v_state": "3"
+}
 
+User2 = {"v_first_name": "Kamil", "v_last_name": "Testowy", "v_email": "kamil@o2.pl", "v_password": "secret22"}
 
 @pytest.fixture
 def browser():
     driver = Firefox()
     driver.implicitly_wait(20)
     yield driver
-    driver.quit()
+    #driver.quit()
 
-
+'''
 def test_basic_register(browser):
-    User1 = {
-        "v_first_name": "Kamik",
-        "v_last_name": "Hahahda",
-        "v_email": "kamil@hdajjhaha.pl",
-        "v_password": "qwedrty",
-        "v_address": "Zidelna 123",
-        "v_city": "Wroclaw",
-        "v_postal": "12555",
-        "v_country": "21",
-        "v_mobile_phone": "89837484783",
-        "v_alias": "My Address 1d",
-        "v_gender": 2,
-        "v_day": "12",
-        "v_month": "1",
-        "v_year": "1972",
-        "v_state": "3"
 
-    }
     create_an_account_page = CreateAccountByPuttingEmail(browser)
     create_an_account_page.load()
     create_an_account_page.register(User1.get("v_email"))
@@ -43,3 +44,16 @@ def test_basic_register(browser):
     file = open('log.txt', 'w')
     file.write(browser.current_url)
     file.close()
+'''
+
+def test_log_in(browser):
+    create_an_account_page = CreateAccountByPuttingEmail(browser)
+    create_an_account_page.load()
+
+    # check if a user is logged if not log in
+    #
+    #
+
+    create_an_account_page.log_in(User2)
+    # check again
+
